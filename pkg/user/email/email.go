a53b757c3106e5b6be347807b0ca123c8adb7949
Dudeiebot <ojoexcel5@gmail.com>
2024-08-07 16:50:31 +0100

    improve readability

--- pkg/user/email/email.go
+++ pkg/user/email/email.go
@@ -9,8 +9,13 @@ import (
 	emailNew "github.com/jordan-wright/email"
 )
 
-func SendVerificationEmail(recipientEmail, verificationToken string, req *http.Request,
-) error {
+type UserInfo struct {
+	RecipientEmail    string
+	VerificationToken string
+	Req               *http.Request
+}
+
+func SendVerificationEmail(info *UserInfo) error {
 	var (
 		fromEmail     = os.Getenv("FROM")
 		smtpServer    = os.Getenv("SMTP_SERVER")
@@ -18,22 +23,22 @@ func SendVerificationEmail(recipientEmail, verificationToken string, req *http.R
 		postmarkToken = os.Getenv("POSTMARK_TOKEN")
 	)
 
-	host := req.Host
+	host := info.Req.Host
 	scheme := "http"
-	if req.TLS != nil {
+	if info.Req.TLS != nil {
 		scheme = "https"
 	}
 
 	e := emailNew.NewEmail()
 	e.From = fmt.Sprintf("<%s>", fromEmail)
-	e.To = []string{recipientEmail}
+	e.To = []string{info.RecipientEmail}
 	e.Subject = "Email Verification Link"
 	e.Text = []byte(
 		fmt.Sprintf(
 			"Please verify your email by clicking the link: %s://%s/auth/verify-email?token=%s",
 			scheme,
 			host,
-			verificationToken,
+			info.VerificationToken,
 		),
 	)
 
