810b5573a81b77c4d2434a3141f46eea7597d894
Dudeiebot <ojoexcel5@gmail.com>
2024-08-18 19:11:56 +0100

    removing some function and making all things equal

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -4,90 +4,72 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
-	"log"
 	random "math/rand"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
-	"github.com/go-chi/chi/v5"
 	"golang.org/x/crypto/bcrypt"
-
-	"github.com/dudeiebot/sportPeerGo/pkg/dbs"
-	"github.com/dudeiebot/sportPeerGo/pkg/user/email"
-	"github.com/dudeiebot/sportPeerGo/pkg/user/model"
-	"github.com/dudeiebot/sportPeerGo/pkg/user/queries"
 )
 
-func UserRoutes(r chi.Router, dbs *dbs.Service) {
-	r.Route("/auth", func(r chi.Router) {
-		r.Post("/register", createUser(dbs))
-		r.Post("/login", loginUser(dbs))
-		r.Post("/logout", logoutUser(dbs))
-		r.Post("/verify-otp", verifyOtp(dbs))
-		r.Get("/verify-email", email.VerifyEmail(dbs))
-	})
-}
+// func createUser(dbService *dbs.Service) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		var user model.User
+// 		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+// 			http.Error(w, err.Error(), http.StatusBadRequest)
+// 			return
+// 		}
+//
+// 		if err := user.ValidateUser(); err != nil {
+// 			http.Error(w, err.Error(), http.StatusBadRequest)
+// 			return
+// 		}
+//
+// 		pwbytes := []byte(user.Password)
+// 		hashedPass, err := bcrypt.GenerateFromPassword(pwbytes, bcrypt.DefaultCost)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusInternalServerError)
+// 			return
+// 		}
+//
+// 		user.Password = string(hashedPass)
+// 		user.Username = generateUsername(user.Email)
+// 		user.VerificationToken, err = verificationToken()
+// 		if err != nil {
+// 			http.Error(w, "Failed to generate verification token", http.StatusInternalServerError)
+// 			return
+// 		}
+//
+// 		user.ID = queries.RegisterQuery(user, dbService)
+// 		if user.ID == 0 {
+// 			http.Error(w, "Failed to register user", http.StatusInternalServerError)
+// 			return
+// 		}
+//
+// 		info := &email.UserInfo{
+// 			RecipientEmail:    user.Email,
+// 			VerificationToken: user.VerificationToken,
+// 			Req:               r,
+// 		}
+//
+// 		go func() {
+// 			err = email.SendVerificationEmail(context.Background(), info)
+// 			if err != nil {
+// 				log.Printf("Failed to send verification email: %v", err)
+// 			}
+// 		}()
+//
+// 		w.WriteHeader(http.StatusCreated)
+// 		json.NewEncoder(w).Encode(map[string]interface{}{
+// 			"message": "User registered successfully. Please check your email for the verification link.",
+// 			"user":    user,
+// 		})
+// 	}
+// }
 
-func createUser(dbService *dbs.Service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var user model.User
-		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if err := user.ValidateUser(); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		pwbytes := []byte(user.Password)
-		hashedPass, err := bcrypt.GenerateFromPassword(pwbytes, bcrypt.DefaultCost)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		user.Password = string(hashedPass)
-		user.Username = generateUsername(user.Email)
-		user.VerificationToken, err = verificationToken()
-		if err != nil {
-			http.Error(w, "Failed to generate verification token", http.StatusInternalServerError)
-			return
-		}
-
-		user.ID = queries.RegisterQuery(user, dbService)
-		if user.ID == 0 {
-			http.Error(w, "Failed to register user", http.StatusInternalServerError)
-			return
-		}
-
-		info := &email.UserInfo{
-			RecipientEmail:    user.Email,
-			VerificationToken: user.VerificationToken,
-			Req:               r,
-		}
-
-		go func() {
-			err = email.SendVerificationEmail(context.Background(), info)
-			if err != nil {
-				log.Printf("Failed to send verification email: %v", err)
-			}
-		}()
-
-		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"message": "User registered successfully. Please check your email for the verification link.",
-			"user":    user,
-		})
-	}
-}
-
-func verificationToken() (string, error) {
+func VerificationToken() (string, error) {
 	bytes := make([]byte, 16)
 	_, err := rand.Read(bytes)
 	if err != nil {
@@ -96,7 +78,7 @@ func verificationToken() (string, error) {
 	return hex.EncodeToString(bytes), nil
 }
 
-func generateUsername(email string) string {
+func GenerateUsername(email string) string {
 	parts := strings.Split(email, "@")
 	if len(parts) > 1 {
 		baseUsername := parts[0]
@@ -107,26 +89,46 @@ func generateUsername(email string) string {
 	return ""
 }
 
-func loginUser(dbService *dbs.Service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		userID := chi.URLParam(r, "userID")
-		// Your logic to get a user by ID
-		w.Write([]byte("Get User by ID: " + userID))
+func EncryptAuth(pass string) (string, error) {
+	hashedAuth, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
 	}
+	return string(hashedAuth), nil
 }
 
-func logoutUser(dbService *dbs.Service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		userID := chi.URLParam(r, "userID")
-		// Your logic to update a user by ID
-		w.Write([]byte("Update User by ID: " + userID))
-	}
+func AddHostSchemeMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		scheme := "http"
+		if r.TLS != nil {
+			scheme = "https"
+		}
+		ctx := context.WithValue(r.Context(), "host", r.Host)
+		ctx = context.WithValue(ctx, "scheme", scheme)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
-func verifyOtp(dbService *dbs.Service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		userID := chi.URLParam(r, "userID")
-		// Your logic to delete a user by ID
-		w.Write([]byte("Delete User by ID: " + userID))
-	}
-}
+// func loginUser(dbService *dbs.Service) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		userID := chi.URLParam(r, "userID")
+// 		// Your logic to get a user by ID
+// 		w.Write([]byte("Get User by ID: " + userID))
+// 	}
+// }
+//
+// func logoutUser(dbService *dbs.Service) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		userID := chi.URLParam(r, "userID")
+// 		// Your logic to update a user by ID
+// 		w.Write([]byte("Update User by ID: " + userID))
+// 	}
+// }
+//
+// func verifyOtp(dbService *dbs.Service) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		userID := chi.URLParam(r, "userID")
+// 		// Your logic to delete a user by ID
+// 		w.Write([]byte("Delete User by ID: " + userID))
+// 	}
+// }
