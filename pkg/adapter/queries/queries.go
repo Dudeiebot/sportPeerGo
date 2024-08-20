583a3be88e90e31c85d4382311c6493264836ea2
Dudeiebot <ojoexcel5@gmail.com>
2024-08-20 23:25:15 +0100

    getting auth queries

--- pkg/adapter/queries/queries.go
+++ pkg/adapter/queries/queries.go
@@ -3,6 +3,7 @@ package queries
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"log"
 
 	"github.com/dudeiebot/sportPeerGo/pkg/adapter/dbs"
@@ -25,6 +26,21 @@ func RegisterQuery(u model.User, d *dbs.Service) int {
 	id, _ := res.LastInsertId()
 	u.ID = int(id)
 	return u.ID
+}
+
+func GetHashedAuth(ctx context.Context, c model.Credentials, d *dbs.Service) (*model.User, error) {
+	var user model.User
+	queri := `SELECT id, email, phone, password, is_verified FROM users WHERE email = ? OR phone = ? LIMIT 1`
+	err := d.DB.QueryRowContext(ctx, queri, c.Access, c.Access).
+		Scan(&user.ID, &user.Email, &user.Phone, &user.Password, &user.IsVerified)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// have your custom error here
+			return nil, errors.New("User Not Found")
+		}
+		return nil, err
+	}
+	return &user, nil
 }
 
 func VerifyEmailQueries(ctx context.Context, d *dbs.Service, token string) (sql.Result, error) {
