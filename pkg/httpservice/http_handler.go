c51f0199504c7cd3f36a32ae58f24ab3805e2e95
Dudeiebot <ojoexcel5@gmail.com>
2024-09-02 23:15:57 +0100

    new handler that is handling update Username and email

--- pkg/httpservice/http_handler.go
+++ pkg/httpservice/http_handler.go
@@ -2,9 +2,16 @@ package httpservice
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"reflect"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/dudeiebot/sportPeerGo/pkg/adapter/dbs"
+	"github.com/dudeiebot/sportPeerGo/pkg/user/model"
 )
 
 func NewHandler[IN, OUT any](
@@ -55,4 +62,39 @@ func NewHandler[IN, OUT any](
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(out)
 	}
+}
+
+func NewUpdateHandler(
+	s *Server,
+	queryFunc func(ctx context.Context, dbs *dbs.Service, user model.User) (sql.Result, error),
+	successMessage string,
+) http.HandlerFunc {
+	return NewHandler(func(ctx context.Context, req *http.Request) (*Response, error) {
+		id := chi.URLParam(req, "id")
+		userId := ctx.Value("userId").(int64)
+		idInt, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			return &Response{Message: "invalid user ID format"}, nil
+		}
+		if idInt != userId {
+			return &Response{Message: "Unauthorized"}, nil
+		}
+		var user model.User
+		if err := json.NewDecoder(req.Body).Decode(&user); err != nil {
+			return &Response{Message: "invalid request body"}, nil
+		}
+		user.ID = int(userId)
+		res, err := queryFunc(ctx, s.DBS, user)
+		if err != nil {
+			return &Response{Message: "Error executing DB query"}, nil
+		}
+		rowsAffected, err := res.RowsAffected()
+		if err != nil {
+			return &Response{Message: "Error getting rows affected"}, err
+		}
+		if rowsAffected == 0 {
+			return &Response{Message: "No rows affected"}, nil
+		}
+		return &Response{Message: successMessage}, nil
+	})
 }
