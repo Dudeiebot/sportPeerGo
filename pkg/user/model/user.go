d6464a83b18c7a74cfefd469138e6f966ce79325
Dudeiebot <ojoexcel5@gmail.com>
2024-09-10 23:50:54 +0100

    validating user input the normal way

--- pkg/user/model/user.go
+++ pkg/user/model/user.go
@@ -1,11 +1,11 @@
 package model
 
 import (
+	"fmt"
+	"net/mail"
+	"regexp"
+	"strings"
 	"time"
-
-	"github.com/go-playground/validator/v10"
-
-	"github.com/dudeiebot/sportPeerGo/pkg/errors"
 )
 
 type User struct {
@@ -32,18 +32,75 @@ type ForgetPass struct {
 }
 
 func (u *User) ValidateUser() error {
-	rules := map[string]string{
-		"Email":    "required,email",
-		"Phone":    "required,e164",
-		"Password": "required,gte=6",
+	var errors []string
+
+	for field, value := range map[string]string{
+		"Email":    u.Email,
+		"Phone":    u.Phone,
+		"Password": u.Password,
+	} {
+		switch field {
+		case "Email":
+			if value == "" {
+				errors = append(errors, "Email is required")
+			} else if !isValidEmail(value) {
+				errors = append(errors, "Invalid email format")
+			}
+		case "Phone":
+			if value == "" {
+				errors = append(errors, "Phone is required")
+			} else if !isValidPhone(value) {
+				errors = append(errors, "Invalid phone number format")
+			}
+		case "Password":
+			if value == "" {
+				errors = append(errors, "Password is required")
+			} else if len(value) < 6 {
+				errors = append(errors, "Password must be at least 6 characters long")
+			}
+		}
 	}
 
-	valid := validator.New()
-	valid.RegisterStructValidationMapRules(rules, u)
-
-	err := valid.Struct(u)
-	if err != nil {
-		return errors.ValidatorErrors(err)
+	if len(errors) > 0 {
+		return fmt.Errorf("Validation errors: %s", strings.Join(errors, ", "))
 	}
 	return nil
+}
+
+func (c *Credentials) ValidateCred() error {
+	var errors []string
+	for field, value := range map[string]string{
+		"Access":   c.Access,
+		"Password": c.Password,
+	} {
+		switch field {
+		case "Access":
+			if value == "" {
+				errors = append(errors, "Email or Phone is required")
+			} else if !isValidEmail(c.Access) && !isValidPhone(c.Access) {
+				errors = append(errors, "Access must be a valid email or phone number")
+			}
+		case "Password":
+			if value == "" {
+				errors = append(errors, "Password is required")
+			} else if len(value) < 6 {
+				errors = append(errors, "Password must be at least 6 characters long")
+			}
+		}
+	}
+	if len(errors) > 0 {
+		return fmt.Errorf("Validation errors: %s", strings.Join(errors, ", "))
+	}
+	return nil
+}
+
+func isValidEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
+
+func isValidPhone(phone string) bool {
+	pattern := `^\+[1-9]\d{1,14}$`
+	match, _ := regexp.MatchString(pattern, phone)
+	return match
 }
