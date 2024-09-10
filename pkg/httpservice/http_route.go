128625bde79d18cf4435cb427c004e39a0a918a8
Dudeiebot <ojoexcel5@gmail.com>
2024-09-10 23:48:40 +0100

    add host and scheme middleware removed

--- pkg/httpservice/http_route.go
+++ pkg/httpservice/http_route.go
@@ -8,7 +8,7 @@ import (
 
 func AuthRoutes(r chi.Router, s *Server) {
 	r.Route("/auth", func(r chi.Router) {
-		r.Post("/register", user.AddHostSchemeMiddleware(CreateUser(s)))
+		r.Post("/register", CreateUser(s))
 		r.Post("/login", LoginUser(s))
 		r.Post("/logout", LogoutUser(s))
 		r.Put("/updatepass", VerifyOtpAndUpdatePass(s))
@@ -20,6 +20,6 @@ func UserRoute(r chi.Router, s *Server) {
 	r.Route("/users", func(r chi.Router) {
 		r.Put("/username/{id}", user.AuthMiddleware(UpdateUsername(s)))
 		r.Put("/email/{id}", user.AuthMiddleware(UpdateEmail(s)))
-		r.Post("/forgot-password/{email}", user.AddHostSchemeMiddleware(SendOtp(s)))
+		r.Post("/forgot-password/{email}", SendOtp(s))
 	})
 }
